ed527346cc495516dcd3a610470f62b75a95ba0d
Jack Hogan <jackhogan11@gmail.com>
2024-09-06 11:40:42 -0400

    Made it look much better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,14 +25,37 @@
 	});
 </script>
 
-<h1 class="font-bold text-6xl">SCRIBE</h1>
-<input type="number" bind:value={xOffset} />
-<input type="number" bind:value={fontSize} />
-<textarea on:change={(_) => {}} bind:value={contents} />
-<p>This only supports letters at the moment</p>
-{#if download !== null}
-	<a download="text.svg" href={download}>DOWNLOAD</a>
-{/if}
+<div
+	class="content-center flex flex-col items-center outline-amber-400 outline-2 outline m-2 p-5 [&>*]:m-1"
+>
+	<h1 class="font-bold text-6xl">SCRIBE</h1>
+	<p>A utility to generate isometric SVGs of arbitrary text.</p>
+	<div class="inline">
+		<label for="offset" class="mr-3">X Offset:</label>
+		<input id="offset" type="number" bind:value={xOffset} />
+	</div>
+	<div class="inline">
+		<label for="font" class="mr-3">Font Size:</label>
+		<input id="font" type="number" bind:value={fontSize} />
+	</div>
+	<div class="flex flex-col items-center">
+		<label for="contents" class="mb-3">Contents:</label>
+		<textarea id="contents" bind:value={contents} />
+	</div>
+	<p class="text-sm">⚠️ This only supports letters at the moment.</p>
+	{#if download !== null}
+		<a download="text.svg" href={download} class="p-3 bg-gray-200 rounded-md hover:bg-gray-400"
+			>DOWNLOAD</a
+		>
+	{/if}
+	<p class="text-xs text-gray-500">
+		🧱 A Purdue Hackers Project · <a
+			href="https://github.com/PurdueHackers/scribe"
+			class="underline"
+			target="_blank">Open Source</a
+		>
+	</p>
+</div>
 
 {@html data}
 
