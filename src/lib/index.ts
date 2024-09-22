defced4ba05599eb89cfc2f96bf2ff3c8789c790
Jack Hogan <jackhogan11@gmail.com>
2024-09-21 22:35:11 -0400

    New font, kerning

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-import font from './PHACKISOMETRIC.otf?base64';
+import font from './PHACKISOMETRIC.ttf?base64';
 import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js';
 
 export function generateSvg(
@@ -24,18 +24,30 @@ export function generateSvg(
 	const draw = SVG()
 		.size('100%', '100%')
 		.viewbox(`0 0 ${maxWidth * offset} ${(lines.length + 1) * fontSize - minY}`);
-	draw.fontface('HackFont', `url("data:application/x-font-otf;base64,${font}")`);
+	draw.fontface('HackFont', `url("data:application/x-font-ttf;base64,${font}")`);
 	let line = 0;
 	for (const con of lines) {
 		let i = 0;
+		let accumulator = 0;
 		for (const char of con) {
+			const kernedOffset = (function o(c: string) {
+				if (c === 'i' || c === 'l') {
+					return (offset * 1300) / 2048;
+				} else if (c === 'f' || c === 'j') {
+					return (offset * 1700) / 2048;
+				} else {
+					return offset;
+				}
+			})(char);
 			const text = draw.text(char);
 			// Extra offset for lowercase drop characters like p
 			text
-				.move(i * offset, fontSize + line * (fontSize + 10) - i * yOffset - minY)
+				.move(accumulator, fontSize + line * (fontSize + 10) - i * yOffset - minY)
 				.font({ family: 'HackFont', size: fontSize })
 				.fill(color);
 			i += 1;
+
+			accumulator += kernedOffset;
 		}
 		line += 1;
 	}
